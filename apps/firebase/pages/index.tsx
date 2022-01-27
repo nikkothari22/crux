83b1c6e4a08b335de5d8662b1fb0f24f47716651
Nikhil Kothari <nik.kothari22@live.com>
2022-01-27 19:32:11 +0700

    Removed button import

--- apps/firebase/pages/index.tsx
+++ apps/firebase/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+// import { Button } from "ui";
 
 export default function Docs() {
   return (
     <div>
       <h1>Firebase</h1>
-      <Button />
+      {/* <Button /> */}
     </div>
   );
 }
